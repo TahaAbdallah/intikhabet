51139e431e561e9aaa100494a12cbaab46c03150
Taha Abdallah <taha.abdallah1999@gmail.com>
2022-02-11 14:31:44 +0200

    intikhabet project - nav finished

---
+++ public/js/hide-nav.js
@@ -0,0 +1,20 @@
+function showNav() {
+    var nav2 = document.getElementById("nav-2")
+    var closeBtn = document.getElementById("close-btn")
+    var burgerBtn = document.getElementById("burger-btn")
+
+    nav2.classList.toggle("show-nav-2");
+    closeBtn.classList.toggle("hide-btn");
+    burgerBtn.classList.toggle("hide-btn");
+}
+
+function hideNav() {
+    var nav2 = document.getElementById("nav-2")
+    var closeBtn = document.getElementById("close-btn")
+    var burgerBtn = document.getElementById("burger-btn")
+
+    nav2.classList.toggle("show-nav-2");
+    closeBtn.classList.toggle("hide-btn");
+    burgerBtn.classList.toggle("hide-btn");
+}
+
