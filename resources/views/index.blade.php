6effa73a06babade9bb880511d91c2deb7d2b7f8
Taha Abdallah <taha.abdallah1999@gmail.com>
2022-02-15 15:43:29 +0200

    testing on phones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,20 +2,26 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>Election - Homepage</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <script src="https://use.fontawesome.com/393b5419bd.js"></script>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/normalise.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
 
     <div class="main-bg">
+
+        {{-- ## START NAVBAR ## --}}
+
         <nav>
             <div class="nav-1 row align-items-center justify-content-between">
                 <div>
@@ -27,7 +33,10 @@
                 </div>
                 <div>
                     <div class="row align-items-center">
-                        <p class="p-0 m-0 profile-name">طه عبدالله</p>
+                        <div class="text-right">
+                            <p class="p-0 m-0 profile-name">طه عبدالله</p>
+                            <p class="p-0 m-0 role">دورك : مسؤول</p>
+                        </div>
                         <div class="profile-img-cont">
                             <img class="profile-img" src="{{ asset('images/IMG_20211212_164914_346.jpg') }}">
                         </div>
@@ -48,35 +57,17 @@
                     </div>
 
                     <div class="nav-2-tables">
-                        <p>المستخدمين</p>
+                        <p>لوائح الشّطب</p>
                         <div class="nav-2-tables-content">
-                            <a href="">Data Entry</a>
-                            <a href="">المراقبين</a>
-                            <a href="">المندوبين متجولين</a>
-                            <a href="">المندوبين الثابتين</a>
-                            <a href="">المنسقين</a>
+                            <a href="">المنتخِبين</a>
+                            <a href="">لم ينتخبوا بعد</a>
                         </div>
                     </div>
 
                     <div class="nav-2-tables">
-                        <p>المستخدمين</p>
+                        <p>المرشّحين</p>
                         <div class="nav-2-tables-content">
-                            <a href="">Data Entry</a>
-                            <a href="">المراقبين</a>
-                            <a href="">المندوبين متجولين</a>
-                            <a href="">المندوبين الثابتين</a>
-                            <a href="">المنسقين</a>
-                        </div>
-                    </div>
-
-                    <div class="nav-2-tables">
-                        <p>المستخدمين</p>
-                        <div class="nav-2-tables-content">
-                            <a href="">Data Entry</a>
-                            <a href="">المراقبين</a>
-                            <a href="">المندوبين متجولين</a>
-                            <a href="">المندوبين الثابتين</a>
-                            <a href="">المنسقين</a>
+                            <a href="">جميع المرشّحين</a>
                         </div>
                     </div>
 
@@ -87,7 +78,185 @@
 
 
         </nav>
+        {{-- ## END NAVBAR ## --}}
+
+        <div class="container row align-items-center justify-content-around m-auto main-cont">
+            <input class="search-bar" type="search" name="" id="" placeholder="بحث">
+            <a class="adding-btn" href="">اضافة مُنتَخِب <i class="fa fa-plus" aria-hidden="true"></i></a>
+        </div>
+
+        <div class="row justify-content-center">
+
+            <div class="container-fluid table-title-cont">
+                <h5 class="table-title">لوائح الشّطب</h5>
+            </div>
+
+            <div class="container-fluid row justify-content-between table-content-cont">
+
+                <div>
+                    <p class="table-content-title">ملاحظة</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                    <p class="table-content-contents">لا يوجد ملاحظات</p>
+                </div>
+                <div>
+                    <p class="table-content-title">رقم الهاتف</p>
+                    <p class="table-content-contents">86123456</p>
+                    <p class="table-content-contents">71123456</p>
+                    <p class="table-content-contents">86123456</p>
+                    <p class="table-content-contents">71123456</p>
+                    <p class="table-content-contents">86123456</p>
+                    <p class="table-content-contents">71123456</p>
+                    <p class="table-content-contents">86123456</p>
+                    <p class="table-content-contents">71123456</p>
+                    <p class="table-content-contents">86123456</p>
+                    <p class="table-content-contents">71123456</p>
+                </div>
+                <div>
+                    <p class="table-content-title">انتخب / لم ينتخب</p>
+                    <p class="table-content-contents">انتخب</p>
+                    <p class="table-content-contents">لم ينتخب</p>
+                    <p class="table-content-contents">انتخب</p>
+                    <p class="table-content-contents">لم ينتخب</p>
+                    <p class="table-content-contents">انتخب</p>
+                    <p class="table-content-contents">لم ينتخب</p>
+                    <p class="table-content-contents">انتخب</p>
+                    <p class="table-content-contents">لم ينتخب</p>
+                    <p class="table-content-contents">انتخب</p>
+                    <p class="table-content-contents">لم ينتخب</p>
+                </div>
+                <div>
+                    <p class="table-content-title">القضاء</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                    <p class="table-content-contents">طرابلس</p>
+                </div>
+                <div>
+                    <p class="table-content-title">المحافظة</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                    <p class="table-content-contents">الشمال</p>
+                </div>
+                <div>
+                    <p class="table-content-title">رقم القيد</p>
+                    <p class="table-content-contents">217</p>
+                    <p class="table-content-contents">71</p>
+                    <p class="table-content-contents">217</p>
+                    <p class="table-content-contents">71</p>
+                    <p class="table-content-contents">217</p>
+                    <p class="table-content-contents">71</p>
+                    <p class="table-content-contents">217</p>
+                    <p class="table-content-contents">71</p>
+                    <p class="table-content-contents">217</p>
+                    <p class="table-content-contents">71</p>
+                </div>
+                <div>
+                    <p class="table-content-title">المذهب</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                    <p class="table-content-contents">مسلم سنّي</p>
+                </div>
+                <div>
+                    <p class="table-content-title">تاريخ الولادة</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                    <p class="table-content-contents">30/4/1999</p>
+                </div>
+                <div>
+                    <p class="table-content-title">الجنس</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                    <p class="table-content-contents">ذكر</p>
+                </div>
+                <div>
+                    <p class="table-content-title">اسم الأم</p>
+                    <p class="table-content-contents">ميرنا</p>
+                    <p class="table-content-contents">سميرة</p>
+                    <p class="table-content-contents">ميرنا</p>
+                    <p class="table-content-contents">سميرة</p>
+                    <p class="table-content-contents">ميرنا</p>
+                    <p class="table-content-contents">سميرة</p>
+                    <p class="table-content-contents">ميرنا</p>
+                    <p class="table-content-contents">سميرة</p>
+                    <p class="table-content-contents">ميرنا</p>
+                    <p class="table-content-contents">سميرة</p>
+                </div>
+                <div>
+                    <p class="table-content-title">اسم الأب</p>
+                    <p class="table-content-contents">نضال</p>
+                    <p class="table-content-contents">رامي</p>
+                    <p class="table-content-contents">نضال</p>
+                    <p class="table-content-contents">رامي</p>
+                    <p class="table-content-contents">نضال</p>
+                    <p class="table-content-contents">رامي</p>
+                    <p class="table-content-contents">نضال</p>
+                    <p class="table-content-contents">رامي</p>
+                    <p class="table-content-contents">نضال</p>
+                    <p class="table-content-contents">رامي</p>
+                </div>
+                <div>
+                    <p class="table-content-title">الاسم</p>
+                    <p class="table-content-contents">طه عبدالله</p>
+                    <p class="table-content-contents">كريم كمالي</p>
+                    <p class="table-content-contents">طه عبدالله</p>
+                    <p class="table-content-contents">كريم كمالي</p>
+                    <p class="table-content-contents">طه عبدالله</p>
+                    <p class="table-content-contents">كريم كمالي</p>
+                    <p class="table-content-contents">طه عبدالله</p>
+                    <p class="table-content-contents">كريم كمالي</p>
+                    <p class="table-content-contents">طه عبدالله</p>
+                    <p class="table-content-contents">كريم كمالي</p>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
+
+
 
 
 
--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -2,14 +2,17 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>Election - Login Page</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <script src="https://use.fontawesome.com/393b5419bd.js"></script>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/normalise.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
